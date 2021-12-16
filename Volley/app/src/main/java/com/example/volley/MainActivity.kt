56cc41192384f5aa1186ff8130d074c83c3d1032
WajeehAwan <80529094+WajeehAwan@users.noreply.github.com>
2021-12-16 20:51:27 +0500

    Update MainActivity.kt

--- Volley/app/src/main/java/com/example/volley/MainActivity.kt
+++ Volley/app/src/main/java/com/example/volley/MainActivity.kt
@@ -9,9 +9,6 @@ import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import android.graphics.Bitmap
-import android.media.Image
-import android.widget.ImageView
-import com.android.volley.Request.Method.GET
 
 import com.android.volley.toolbox.ImageRequest
 
@@ -27,7 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         val textView = findViewById<TextView>(R.id.textview1)
 
-        val btnurl: Button = findViewById(R.id.btnurl)
+        val btnurl:Button=findViewById(R.id.btnurl)
 
         btnurl.setOnClickListener {
             val queue = Volley.newRequestQueue(this)
@@ -35,23 +32,23 @@ class MainActivity : AppCompatActivity() {
             val stringRequest = StringRequest(
                 Request.Method.GET, url,
                 Response.Listener { response ->
-                    textView.text = response.toString()
+                    textView.text=response.toString()
 
-                },
+                                  },
                 Response.ErrorListener { textView.text = "That didn't work!" })
 
             queue.add(stringRequest)
         }
 
 
-        val btnjson: Button = findViewById(R.id.btnjson)
+        val btnjson:Button=findViewById(R.id.btnjson)
         btnjson.setOnClickListener {
             val queue = Volley.newRequestQueue(this)
             val url = "https://run.mocky.io/v3/c4a2cf71-568a-485c-a789-43a5df39e52c"
             val stringRequest = StringRequest(
                 Request.Method.GET, url,
                 Response.Listener { response ->
-                    textView.text = response.toString()
+                    textView.text=response.toString()
 
                 },
                 Response.ErrorListener { textView.text = "That didn't work!" })
@@ -59,27 +56,5 @@ class MainActivity : AppCompatActivity() {
             queue.add(stringRequest)
         }
 
-        ivImageView = (ImageView)findViewById(R.id.);
-        mRequestQueue = Volley.newRequestQueue(this.getApplicationContext());
-        ImageRequest imageRequest = new ImageRequest(url, new BitmapListener(), 0, 0, null, null, new MyErrorListener());
-
-        mRequestQueue.add(imageRequest);
-    }
-
-    private class BitmapListener implements Response.Listener<Bitmap> {
-        @Override
-        public void onResponse(Bitmap response) {
-// response = your url's bitmap
-            ivImageView.setImageBitmap(response);
-
-        }
-    }
-
-    private class MyErrorListener implements Response.ErrorListener {
-        @Override
-        public void onErrorResponse(VolleyError error) {
-//store a default image if connection failed
-            ivImageView.setImageResource(R.drawable.error_icon);
-        }
     }
 }
